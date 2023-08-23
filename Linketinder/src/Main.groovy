050eba2cabefb9a9f5649b3d090fc001fb5b778e
Gabriel de Almeida Martins <gabriel_241@outlook.com.br>
2023-08-23 16:21:37 -0300

    BackEnd-Groovy(T002-Features) feat - Criação e implementação da classe Main

---
+++ Linketinder/src/Main.groovy
@@ -0,0 +1,38 @@
+import com.linketinder.features.PessoaFisicaFeatures
+import com.linketinder.features.PessoaJuridicaFeatures
+
+PessoaFisicaFeatures candidato = new PessoaFisicaFeatures()
+PessoaJuridicaFeatures empresa = new PessoaJuridicaFeatures()
+
+boolean repeat = true
+
+do{
+    println ("-LinkeTinder-\nEscolha uma opção:\n1 - Listar candidatos\n2 - Listar empresas\n3 - Cadastrar sua empresa\n4 - Cadastre-se\n5 - Sair")
+    try{
+        int opcao = System.in.newReader().readLine() as int
+
+        switch (opcao){
+            case 1:
+                //candidato.listarCandidatos()
+                break
+            case 2:
+                //empresa.listarEmpresas()
+                break
+            case 3:
+                //empresa.cadastrarEmpresas()
+                break
+            case 4:
+                //candidato.cadastrarCandidatos()
+                break
+            case 5:
+                repeat = false
+                break
+            default:
+                println("OPÇÃO INVÁLIDA")
+                break
+        }
+
+    } catch (Exception ignored){
+        println("OPÇÃO INVÁLIDA")
+    }
+}while (repeat)
